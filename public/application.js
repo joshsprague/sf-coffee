9a78fac819035813ec0289d476e3928d278926e0
joshsprague <jjspragu@asu.edu>
2015-01-18 20:11:12 -0800

    Setup application.js and hash prefix

---
+++ public/application.js
@@ -0,0 +1,15 @@
+var mainAppModuleName = 'sf-coffee';
+
+var mainAppModule = angular.module(mainAppModuleName, []);
+
+mainAppModule.config(['$locationProvider',
+  function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }
+]);
+
+if (window.location.hash === '#_=_') window.location.hash = '#!';
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, [mainAppModuleName]);
+});
