5d98886a37dca4a4123b02246f427d0c6c04957b
joshsprague <jjspragu@asu.edu>
2015-01-21 11:50:30 -0800

    Leaflet added; map displays and centers on SF

--- public/cafes/config/cafes.client.routes.js
+++ public/cafes/config/cafes.client.routes.js
@@ -3,6 +3,9 @@ angular.module('cafes').config(['$routeProvider', function($routeProvider) {
     .when('/cafes', {
       templateUrl: 'cafes/views/list-cafes.client.view.html'
     })
+    .when('/map', {
+      templateUrl: 'cafes/views/map.client.view.html'
+    })
     .otherwise({
       redirectTo: '/'
     });
--- public/cafes/controllers/cafes.client.controller.js
+++ public/cafes/controllers/cafes.client.controller.js
@@ -5,4 +5,18 @@ angular.module('cafes').controller('CafesController',
     $scope.find = function() {
       $scope.cafes = Cafes.query();
     };
+
+    var loadMap = function() {
+      var layer = new L.StamenTileLayer("toner-lite");
+      var map = new L.Map("map", {
+        center: new L.LatLng(37.75, -122.45),
+        zoom: 12
+      });
+
+      map.addLayer(layer);
+
+      map.locate({setView: true, maxZoom: 16});
+    }
+
+    loadMap();
   }]);
