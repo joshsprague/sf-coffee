8b9d9a2ad1daaaa0daed67402a79fcfa10d0ee9c
joshsprague <jjspragu@asu.edu>
2015-06-09 23:05:03 -0700

    Reformat cafe router

--- public/cafes/config/cafes.client.routes.js
+++ public/cafes/config/cafes.client.routes.js
@@ -1,18 +1,20 @@
-angular.module('cafes').config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/cafes', {
-      templateUrl: 'cafes/views/list-cafes.client.view.html'
-    })
-    .when('/cafes/:cafeId', {
-      templateUrl: 'cafes/views/edit-cafe.client.view.html'
-    })
-    .when('/map', {
-      templateUrl: 'cafes/views/map.client.view.html'
-    })
-    .when('/test', {
-      templateUrl: 'cafes/views/map-test.client.view.html'
-    })
-    .otherwise({
-      redirectTo: '/map'
-    });
+angular
+  .module('cafes')
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/cafes', {
+        templateUrl: 'cafes/views/list-cafes.client.view.html'
+      })
+      .when('/cafes/:cafeId', {
+        templateUrl: 'cafes/views/edit-cafe.client.view.html'
+      })
+      .when('/map', {
+        templateUrl: 'cafes/views/map.client.view.html'
+      })
+      .when('/test', {
+        templateUrl: 'cafes/views/map-test.client.view.html'
+      })
+      .otherwise({
+        redirectTo: '/map'
+      });
 }]);
